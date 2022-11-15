9a5422aecb1ea37ba5c48feda49653bf77602b2b
Pas <askpeevee@gmail.com>
2022-11-15 17:08:21 +0000

    python 100 days challenge

---
+++ sectionOne.py
@@ -0,0 +1,68 @@
+# 1 PRINTING TO CONSOLE
+
+#print("Day 1 - Python print Function")
+#print("The funtion is declared like this")
+#print("what to print")
+
+#Newline in python - \n 
+
+#print("Hello world!\nHello world!\nHello world!")
+
+#concantenation with SPACE
+
+#print("Hello" + " " + "Pas") 
+
+
+# 2 DEBUGGING CODE EXERCISE
+
+#print("Day 1 -String Manipulation")
+
+#print("String Conctenation is done with the" "+" "sign.")
+
+#print('e.g. print("Hello" +"world")')
+
+#print("New lines can be created with a backslash and n.")
+
+# 3 INPUT FUNCTION CODE EXERCISE
+
+#Use input to make code more vaiable
+#Three functions in one block of code
+#print function
+#len function
+#input function
+
+#print(len(input("What is your name? ")))
+
+
+# 4 PYTHON VARIABLE
+
+#name = input("What is your name? ")
+#print(name)
+
+# Using variable to carry out tasks
+
+#name = "Jack"
+#print(name)
+
+#name = "Angel
+
+# Another ways of using variable to carry out tasks
+
+#name = input("What is your name")
+#lenght = len(name)
+#print(lenght)
+
+
+#PYTHON PROJECT
+
+# Create a greeting for a program.
+
+#print("Welcome to our company")
+
+#city = input("Which city were you born?\n") 
+
+#pet = input("What is the name of your pet\n")
+
+#print("Your band name could be " + city + "" + pet)
+
+This is the section one
